61b19557a33b3297ee66716cd2ff9775e0950a2f
LeKieu <94425786+UIT-21522268@users.noreply.github.com>
2024-11-17 22:56:48 +0700

    done nav bar - fetch api

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { TiWeatherPartlySunny } from "react-icons/ti";
+import { GrMapLocation } from "react-icons/gr";
+import { GrLocation } from "react-icons/gr";
+import SearchAppBar from './Searchbar/Searchbar';
+type Props = {}
+
+export default function Navbar({ }: Props) {
+    return (
+        <>
+            <nav className="shadow-sm  sticky top-0 left-0 z-50 bg-white">
+                <div className="h-[80px]     w-full    flex   justify-between items-center  max-w-7xl px-10 mx-auto">
+                    <p className="flex items-center justify-center gap-2  ">
+                        <h2 className="text-teal-400 text-3xl">Weather  </h2>
+                        <TiWeatherPartlySunny className='text-3xl text-yellow-300 mt-1' />
+                    </p>
+                    <section className="flex items-center  gap-5  ">
+                        <GrMapLocation className='text-2xl mt-1 hover:opacity-60 cursor-pointer' />
+                        <GrLocation className='text-2xl mt-1 hover:opacity-60 cursor-pointer' />
+                        <p className='text-teal-900/80 text-sm font-medium '>BMT</p>
+                        <div><SearchAppBar /></div>
+                    </section>
+
+                </div>
+            </nav>
+        </>
+    )
+}
